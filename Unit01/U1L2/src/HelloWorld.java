5eff21b34115ac7d6f9be594177f51b7e5826e10
maddotri000 <maddotri00)@warren.k12.in.us>
2023-08-09 10:49:45 -0400

    U1L2

---
+++ Unit01/U1L2/src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Stop it");
+        System.out.println("Ok I'm done with you");
+    }
+}
