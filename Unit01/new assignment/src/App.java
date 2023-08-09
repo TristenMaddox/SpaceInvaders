29a1aeacd975bc5b9e210c17e43c87ab68bc1dad
maddotri000 <maddotri00)@warren.k12.in.us>
2023-08-09 10:57:06 -0400

    U1L3

---
+++ Unit01/new assignment/src/App.java
@@ -0,0 +1,42 @@
+public class App {
+    public static void main(String[] args) {
+
+
+        int number = 598;
+
+        System.out.println(number);
+
+        long largeNumber = 327664545;
+
+        System.out.println(largeNumber);
+
+        short shortNumber = 128;
+
+        System.out.println(shortNumber);
+
+        byte  byteNumber = 123;
+
+        System.out.println(byteNumber);
+
+        double doubleNumber =  27267272.22;
+
+        System.out.println(doubleNumber);
+
+        float floatNumber = 5.87f;
+
+        System.out.println(floatNumber);
+
+        boolean booleanNumber = true;
+
+        System.out.println(booleanNumber);
+
+        char aCharacter = 'A';
+
+        System.out.println(aCharacter);
+
+        String myString = "Please stop";
+
+        System.out.println(myString);
+    }
+
+}
