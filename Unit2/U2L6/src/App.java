3bae8295e84760b03af246e8e4b2aaa26f5df4a0
maddotri000 <maddotri00)@warren.k12.in.us>
2023-08-25 10:46:59 -0400

    U1L3

---
+++ Unit2/U2L6/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println(15 -((5 * 2) - 3));
+      System.out.println( (15 * 3 + 14) - (17.5 * 7 * 3));
+      System.out.println( (100 * 100 + 1) * (100 + 2) * 5 - 5);
+    }
+}
